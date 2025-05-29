e0ae60570f2e615d17288cd66a61d09e5a48eba5
MArney86 <matthew.h.arney.1986@gmail.com>
2025-05-28 19:12:47 -0500

    Add Character logic and response modal logic

---
+++ m7project/marvel-character-app/src/components/AddCharacter.jsx
@@ -0,0 +1,204 @@
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import APIModal from './ApiModal';
+import axios from 'axios';
+
+export default function AddProduct({loading, setLoading, error, setError}) {
+
+  const [formData, setFormData] = useState({
+    id: 0,
+    name: "",
+    alias: "",
+    powers: "",
+    alignment: "",
+    image_url: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+  const [character, setCharacter] = useState({
+    id: 0,
+    name: "",
+    alias: "",
+    powers: "",
+    alignment: "",
+    image_url: "",
+  });
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const [validated, setValidated] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleRadioChange = (e) => {
+        const { value } = e.target;
+        setFormData({
+            ...formData,
+            alignment: value
+        });
+    }
+    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+            setError("Please fill out all required fields correctly.");
+        } else {
+            try {
+                const response = await axios.post('127.0.0.1:5000/characters', formData);
+                setProduct(response.data);
+                setSubmitted(true);
+                setShowModal(true);
+                setError(null);
+            } catch (err) {
+                setError(`Error submitting the form. Please try again: ${err.message}`);
+                setSubmitted(false);
+            }
+        }
+        setValidated(true);
+    };
+
+    return (
+        <>
+            <Container className="mt-5">
+                <h2>Add New Product</h2>
+                <FormModal character={character} submitted={submitted} showModal={showModal} handleCloseModal={handleCloseModal} request={"post"} error={error} />
+
+                <Form onSubmit={handleSubmit} noValidate validated={validated}>
+                    <Row>
+                        <Col md="6">
+                            <FloatingLabel controlId="formName" label="Name" className="mb-2 mt-2">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Name of the character"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please provide a valid name.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+
+                        <Col md="6">
+                            <FloatingLabel controlId="formAlias" label="Alias" className="mb-2 mt-2">
+                                <Form.Control
+                                    type="Text"
+                                    placeholder="Character's Alias"
+                                    name="alias"
+                                    value={formData.alias}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please provide a valid alias.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col md="4" className='d-flex'>
+                            <Container className='d-flex flex-column justify-content-center align-items-center'>
+                                <Row>
+                                    <Col md="6"><h3>Alignment:</h3></Col>
+                                    <Col md="6">
+                                        <Form.Check
+                                            type="radio"
+                                            name="alignment"
+                                            value="hero"
+                                            id="heroRadio"
+                                            label="Hero"
+                                            onChange={handleRadioChange}
+                                            isInvalid={formData.alignment === ""}
+                                            feedback="Please choose an alignment"
+                                            feedbackType='invalid'
+                                        />
+                                        <Form.Check
+                                            type="radio"
+                                            name="alignment"
+                                            value="villain"
+                                            id="villainRadio"
+                                            lable="Villain"
+                                            onChange={handleRadioChange}
+                                            isInvalid={formData.alignment === ""}
+                                            feedback="Please choose an alignment"
+                                            feedbackType='invalid'
+                                        />
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Col>
+
+                        <Col md="8">
+                            <FloatingLabel controlId="formPowers" label="Powers" className="mb-2 mt-2">
+                                <Form.Control
+                                    as="textarea"
+                                    name="powers"
+                                    placeholder="Please enter the character's powers in a comma seperated list."
+                                    value={formData.powers}
+                                    onChange={handleChange}
+                                    style={{height:'6.25rem'}}
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter the powers of your character in a comma seperated list.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col md="12">
+                            <FloatingLabel controlId="formImageURL" label="Character Image URL" className="mb-2 mt-2">
+                                <Form.Control
+                                    type="url"
+                                    placeholder="Please enter the URL of the character's image"
+                                    name="image_url"
+                                    value={formData.image_url}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    Please enter a URL of the image of your character.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md="12">
+                        <Alert show={!!error} variant="danger">
+                            <Alert.Heading>!!!Error!!!</Alert.Heading>
+                            <p>{error}</p>
+                        </Alert>
+                        </Col>
+                    </Row>
+
+                    <Button variant="primary" type="submit" className="mt-3">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </>
+    );
+}
---
+++ m7project/marvel-character-app/src/components/FormModal.jsx
@@ -0,0 +1,77 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table';
+
+export default function FormModal({ character, submitted, showModal, handleCloseModal, request, error  }) {
+    let aknowldegement = "";
+    if (request === "post") {
+        aknowldegement = "Character added successfully!";
+    } else if (request === "put") {
+        aknowldegement = "Character updated successfully!";
+    } else if (request === "delete") {
+        aknowldegement = "Character deleted successfully!";
+    }
+
+    const getPowers = () => {
+            return character.powers.split(", ").map((value, index) => (
+                value.substring(0,4) === "and " ? <span key={index}> {value.substring(4).charAt(0).toUpperCase() + value.substring(5)}</span>:<span key={index}>{value.charAt(0).toUpperCase() + value.substring(1)}</span>
+            ));
+    }
+
+    return (
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Form Submitted!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {submitted && (request !== "delete") ?
+                    <>
+                        <p>{aknowldegement}</p>
+                        <p>Nice job!  Here is the information returned from the API:</p>
+                        <Table hover>
+                            <tbody>
+                                <tr>
+                                    <th>id:</th>
+                                    <td>{character.id}</td>
+                                </tr>
+                                <tr>
+                                    <th>name:</th>
+                                    <td>{character.name}</td>
+                                </tr>
+                                <tr>
+                                    <th>alias:</th>
+                                    <td>{character.alias}</td>
+                                </tr>
+                                <tr>
+                                    <th>alignment:</th>
+                                    <td>{character.alignment}</td>
+                                </tr>
+                                <tr>
+                                    <th>powers:</th>
+                                    <td>{getPowers().map((value, index) => (
+                                        <p key ={index}>{value}</p>
+                                    ))}</td>
+                                </tr>
+                                <tr>
+                                    <th>image:</th>
+                                    <td><img src={character.image_url} alt="Character" style={{width: "200px", height: "300px"}} /></td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </>:<>
+                        <p>{aknowldegement}</p>
+                    </>
+                }
+                {error && <Alert show={!!error} variant="danger">
+                    <Alert.Heading>!!!Error!!!</Alert.Heading>
+                    <p>{error}</p>
+                </Alert>}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="success" onClick={handleCloseModal}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
